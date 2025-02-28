b28ea826dc2be21dc19d570220a7fbab3b0e13d7
Yaroslav Milchenko <zadirakalena@gmail.com>
2025-02-28 13:56:55 +0200

    new table

--- 1 lab/main.js
+++ 1 lab/main.js
@@ -1,3 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const burger = document.querySelector(".burger");
+    const mobileMenu = document.getElementById("mobile-menu");
+
+    burger.addEventListener("click", function () {
+        mobileMenu.classList.toggle("show");
+    });
+});
+
 function toggleMenu() {
     document.getElementById("mobile-menu").classList.toggle("show");
 }
